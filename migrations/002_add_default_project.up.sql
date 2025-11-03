3e0b468d24ed27d49424d1d7ce4703b91fb0a83a
Roman Chudov <roman.chudov@gmail.com>
2025-11-03 05:45:18 +0300

    Migration improved.

--- migrations/002_add_default_project.up.sql
+++ migrations/002_add_default_project.up.sql
@@ -1,4 +1,4 @@
-insert into workflows.projects (name, tenant_id, created_at) values ('Default', 1, now()) on conflict (name) do nothing;
+insert into workflows.projects (name, tenant_id, created_at) values ('Default', (SELECT id FROM workflows.tenants ORDER BY id DESC LIMIT 1), now()) on conflict (name) do nothing;
 
 create table if not exists workflows.project_workflows (
     project_id integer references workflows.projects(id) not null,
