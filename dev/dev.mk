82743881f1f42265d2ec78c968ba48ae8f6acb4d
Roman Chudov <roman.chudov@gmail.com>
2025-11-05 17:50:38 +0300

    "Remove LDAP-related docker compose services and associated make targets"

--- dev/dev.mk
+++ dev/dev.mk
@@ -1,20 +1,13 @@
 _COMPOSE_BASE=docker compose -f dev/docker-compose.yml --project-name ${NAMESPACE} --env-file dev/platform.env
-_COMPOSE_EXT=docker compose -f dev/docker-compose.ext.yml --project-name ${NAMESPACE} --env-file dev/platform.env
 
 dev-up: ## Up the environment in docker compose (base services only)
 	${_COMPOSE_BASE} up -d
 
-dev-up-ext: ## Up the environment in docker compose with LDAP services
-	#./dev/generate-ext.sh
-	${_COMPOSE_EXT} up -d
-
 dev-down: ## Down the environment in docker compose
 	${_COMPOSE_BASE} down --remove-orphans
-	${_COMPOSE_EXT} down --remove-orphans
 
 dev-clean: ## Down the environment in docker compose with image cleanup
 	${_COMPOSE_BASE} down --remove-orphans -v --rmi all
-	${_COMPOSE_EXT} down --remove-orphans -v --rmi all
 
 dev-build-proxy: ## Building floxym-reverse-proxy
 	${_COMPOSE_BASE} build floxym-reverse-proxy
